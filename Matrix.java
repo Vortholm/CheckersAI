58af8bc02d09dae512a444fb9ee500a4711c4d19
Vortholm <wv1.v.m.94@gmail.com>
2018-05-17 18:31:40 +0200

    Algorithms addition

--- Matrix.java
+++ Matrix.java
@@ -238,6 +238,10 @@ public class Matrix {
     private List<Integer> PATH1;
     private List<Integer> PATH2;
     private List<Integer> PATH3;
+    private List<Integer> TPATH;
+    private int PATH1S;
+    private int PATH2S;
+    private int PATH3S;
     
     public Matrix(int START) {
         this.START = START;
@@ -303,35 +307,195 @@ public class Matrix {
         }
         
         PATH1 = new ArrayList<>();
-        bestPath(PATH1, F1.getCoords()[0], F1.getCoords()[1], F1.getColor(), this.matrix);
+        TPATH = new ArrayList<>();
+        bestPath(1, F1.getCoords()[0], F1.getCoords()[1], F1.getColor(), this.matrix);
         if (F2 != null) {
             PATH2 = new ArrayList<>();
-            bestPath(PATH2, F2.getCoords()[0], F2.getCoords()[1], F2.getColor(), this.matrix);
+            bestPath(2, F2.getCoords()[0], F2.getCoords()[1], F2.getColor(), this.matrix);
         }
         if (F3 != null) {
             PATH3 = new ArrayList<>();
-            bestPath(PATH3, F3.getCoords()[0], F3.getCoords()[1], F3.getColor(), this.matrix);
+            bestPath(3, F3.getCoords()[0], F3.getCoords()[1], F3.getColor(), this.matrix);
         }
         MUST_EAT.clear();
     }
     
-    private void bestPath(List<Integer> path, int x, int y, int color, Figura[][] mat) {
+    private void bestPath(int pNum, int x, int y, int color, Figura[][] mat) {
         if (x > 7 || y > 7 || x < 0 || y < 0)
             return;
-        int r[] = mat[x][y].canEat(1);
+        int[] r = mat[x][y].canEat(1);
+        eatUp(r, color, pNum, mat, x, y);
         
+        r = mat[x][y].canEat(2);
+        eatUp(r, color, pNum, mat, x, y);
+        
+        r = mat[x][y].canEat(3);
+        eatUp(r, color, pNum, mat, x, y);
+        
+        r = mat[x][y].canEat(4);
+        eatUp(r, color, pNum, mat, x, y);
+    }
+    
+    private void eatUp(int[] r, int color, int pNum, Figura[][] mat, int x, int y) {
         if (r[0] != -1) {
             for (int i : r)
-                path.add(i);
+                TPATH.add(i);
+            
+            Figura f1 = new Figura(color, x, y);
+            if (mat[x][y].isQueen())
+                f1.becomesQueen();
+            
             mat[x][y] = null;
+            
+            Figura f2 = new Figura(mat[r[0]][r[1]].getColor(), r[0], r[1]);
+            if (mat[r[0]][r[1]].isQueen())
+                f2.becomesQueen();
+            
             mat[r[0]][r[1]] = null;
+            
             mat[r[2]][r[3]] = new Figura(color, r[2], r[3]);
             
-            bestPath(path, r[2], r[3], color, mat);
+            if (f1.isQueen())
+                mat[r[2]][r[3]].becomesQueen();
+            
+            bestPath(pNum, r[2], r[3], color, mat);
+            
+            mat[r[2]][r[3]] = null;
+            mat[r[0]][r[1]] = f2;
+            mat[x][y] = f1;
+            
+            TPATH.remove(TPATH.size()-1);
+            TPATH.remove(TPATH.size()-1);
+            TPATH.remove(TPATH.size()-1);
+            TPATH.remove(TPATH.size()-1);
+        } else {
+            if (pNum == 1) {
+                if (PATH1S == 0) {
+                    if (PATH1.contains(-13)) {
+                        int k = 0;
+                        while (PATH1.get(k) != -13) {
+                            PATH1S++;
+                            k++;
+                        }
+                    } else {
+                        PATH1S = PATH1.size();
+                    }
+                }
+                if (TPATH.size() > PATH1S) {
+                    PATH1.clear();
+                    PATH1S = TPATH.size();
+                    TPATH.stream().forEach(e -> PATH1.add(e));
+                } else if (TPATH.size() == PATH1S){
+                    PATH1.add(-13);
+                    TPATH.stream().forEach(e -> PATH1.add(e));
+                }
+            } else if (pNum == 2) {
+                if (PATH2S == 0) {
+                    if (PATH2.contains(-13)) {
+                        int k = 0;
+                        while (PATH2.get(k) != -13) {
+                            PATH2S++;
+                            k++;
+                        }
+                    } else {
+                        PATH2S = PATH2.size();
+                    }
+                }
+                if (TPATH.size() > PATH2S) {
+                    PATH2.clear();
+                    PATH2S = TPATH.size();
+                    TPATH.stream().forEach(e -> PATH2.add(e));
+                } else if (TPATH.size() == PATH2S){
+                    PATH2.add(-13);
+                    TPATH.stream().forEach(e -> PATH2.add(e));
+                }
+            } else {
+                if (PATH3S == 0) {
+                    if (PATH3.contains(-13)) {
+                        int k = 0;
+                        while (PATH3.get(k) != -13) {
+                            PATH3S++;
+                            k++;
+                        }
+                    } else {
+                        PATH3S = PATH3.size();
+                    }
+                }
+                if (TPATH.size() > PATH3S) {
+                    PATH3.clear();
+                    PATH3S = TPATH.size();
+                    TPATH.stream().forEach(e -> PATH3.add(e));
+                } else if (TPATH.size() == PATH3S){
+                    PATH3.add(-13);
+                    TPATH.stream().forEach(e -> PATH3.add(e));
+                }
+            }
         }
     }
     
     public boolean getMustEat() {
         return MUSTEAT;
+    }
+    
+    public void AI(int color) {
+        if (getMustEat()) {
+            
+        } else {
+            int defCounter = 0;
+            List<Integer> lbm = new ArrayList<>();
+            for (int i = 0;i < 8;i++) {
+                for (int j = 0;j < 8;j++) {
+                    if (matrix[i][j].getColor() == color) {
+                        List<Integer> l = matrix[i][j].getMoves();
+                        if (l.size() > 0) {
+                            if (lbm.size() < 1)
+                                if (l.size() > 2)
+                                    
+                            System.out.println("light");
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    public int bestMove(int x, int y) {
+        List<Integer> p1;
+        int s1 = 0;
+        if (PATH1.size() > 0) {
+            p1 = new ArrayList<>();
+            PATH1.forEach(entity1 -> p1.add(entity1));
+            s1 = PATH1S;
+        }
+        
+        List<Integer> p2;
+        int s2 = 0;
+        if (PATH2.size() > 0) {
+            p2 = new ArrayList<>();
+            PATH2.forEach(entity2 -> p2.add(entity2));
+            s1 = PATH2S;
+        }
+        List<Integer> p3;
+        int s3 = 0;
+        if (PATH3.size() > 0) {
+            p3 = new ArrayList<>();
+            PATH3.forEach(entity3 -> p3.add(entity3));
+            s1 = PATH3S;
+        }
+        
+        return PATH3S;//OVO PROMENITI
+    }
+    
+    public Figura[][] cloneMatrix() {
+        Figura[][] r = new Figura[8][8];
+        for (int i = 0;i < 8;i++) {
+            for (int j = 0;j < 8;j++) {
+                r[i][j] = new Figura(matrix[i][j].getColor(), i, j);
+                
+                if (matrix[i][j].isQueen())
+                    r[i][j].becomesQueen();
+            }
+        }
+        return r;
     }
 }
